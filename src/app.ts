2c8cdb7f216e17319b861e993e2945fbf829af78
eusoncode <git@github.com:eusoncode/Kata.git>
2024-04-11 00:16:13 -0600

    Getting User Input

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-// Code goes here!
+// Access form and input
+const form = document.querySelector('form')! as HTMLFormElement;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+//Define function to fetch address input
+function searchAddressHandler(event:Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+  console.log(enteredAddress);
+  
+
+  // send this to Google's API
+};
+
+//Add a listener
+form.addEventListener('submit', searchAddressHandler);
