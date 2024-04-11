4aac911ae3b4b0a9d734e21cb9305c9ef4fef5f9
eusoncode <git@github.com:eusoncode/Kata.git>
2024-04-11 01:29:08 -0600

    Fetch data using axios from Geogle API

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 // Access form and input
 const form = document.querySelector('form')! as HTMLFormElement;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
@@ -5,14 +7,32 @@ const addressInput = document.getElementById('address')! as HTMLInputElement;
 //API Key
 const GOOGLE_API_KEY = 'AIzaSyBPee_7VIH-bzRceVBOnTo4rDilw6h6wKg';
 
+// define type
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number, lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+}
+
 //Define function to fetch address input
 function searchAddressHandler(event:Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
-  console.log(enteredAddress);
-  
 
-  // send this to Google's API
+  // Send this to Google's API
+  axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+    .then(response => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch locaton')
+      } else {
+        const coordinates = response.data.results[0].geometry.location;  
+        console.log(coordinates);       
+      }
+    })
+    .catch(err => {
+      alert(err.message);
+      console.log(err);
+      
+    })
 };
 
 //Add a listener
